16b11fcbd4fd7217b52624551b95e7afe5c1a42e
SanamAI <152692629+SanamAI@users.noreply.github.com>
2023-12-26 13:50:33 +0500

    Add files via upload

---
+++ exercise 1 to 8.py
@@ -0,0 +1,75 @@
+#exercise 1 simple variable assignment
+name = "Sanam"
+age = 19
+
+print(f"My name is {name} and I am {age} years old.")
+
+
+
+
+#exercise 2 multiple variable assignments
+# Declare and assign integer values to num1 and num2
+num1 = 10
+num2 = 5
+
+# Declare the result variable and add num1 and num2
+result = num1 + num2
+
+# Print the values of all three variables
+print(f"num1: {num1}")
+print(f"num2: {num2}")
+print(f"result: {result}")
+# exercise 3: variable types
+# Declare and assign values to variables
+price = 12.50  # Example float value
+quantity = 5   # Example integer value
+
+# Calculate total cost
+total_cost = price * quantity
+
+# Print values
+print("Price:", price)
+print("Quantity:", quantity)
+print("Total Cost:", total_cost)
+#exercise 4 string concatenation
+# Declare and assign values to string variables
+first_name = "Sanam"
+last_name = "Mallah"
+
+# Concatenate the strings
+full_name = first_name + " " + last_name
+
+# Print the full name
+print(full_name)
+#exercise 5 boolean variables
+# Assuming you have an age variable that contains your age
+age = 20
+is_adult = age > 18
+print(is_adult)
+#exercise 6 interactive user input
+# Get the user's name using the input() function
+user_name = input("Please enter your name: ")
+
+# Print a personalized greeting
+print(f"Hello, {user_name}! Welcome to our platform.")
+#exercise 7 temperature conversion
+# Assign a temperature in Celsius to the variable
+celsius_temperature = 25.0  # For example, 25 degrees Celsius
+
+# Convert Celsius to Fahrenheit using the formula
+fahrenheit_temperature = (celsius_temperature * 9/5) + 32
+
+# Print both temperatures
+print(f"Original Celsius Temperature: {celsius_temperature}°C")
+print(f"Converted Fahrenheit Temperature: {fahrenheit_temperature}°F")
+#exercise 8 swapping variables
+# Declare and assign values to variables
+a = 5
+b = 10
+
+# Swap values without using a temporary variable
+a, b = b, a
+
+# Print the swapped values
+print("Value of a after swapping:", a)
+print("Value of b after swapping:", b)
